baef41483fba9a40aeb7924b39ae6e15399a0f70
hamedvalizadehkelasar <hamedvalizadehkelasar@yahoo.com>
2023-12-29 17:48:37 +0330

    session 22-0227 rename post.html to single.php and prepare it to load common codes

--- sidebar-pgeaside.php
+++ sidebar-pgeaside.php
@@ -1,9 +1,7 @@
 <aside>
-    <!-- <a href=""><img src="<?php bloginfo("template_url") ?>/assets/img/aside-ad1.png" alt="" /></a> -->
     <?php
     if (is_active_sidebar('sidebar-widget-1')) {
         dynamic_sidebar('sidebar-widget-1');
     }
     ?>
-    <!-- <a href=""><img src="<?php bloginfo("template_url") ?>/assets/img/aside-ad2.png" alt="" /></a> -->
 </aside>
---
+++ single.php
@@ -0,0 +1,85 @@
+<html lang="en" dir="ltr">
+
+<head>
+  <?php get_header('head'); ?>
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/style.css" />
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/menu.css" />
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/header.css" />
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/footer.css" />
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/bread-crumb.css" />
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/page-content.css" />
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/page-aside.css" />
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/post.css" />
+</head>
+
+<body>
+  <?php get_header('menu'); ?>
+
+  <div class="recotik-container">
+    <div class="bread-crumb">
+      <?php if (function_exists('rank_math_the_breadcrumbs'))
+        rank_math_the_breadcrumbs(); ?>
+    </div>
+    <section class="page-content">
+      <div class="post-head-image">
+        <img src="<?php bloginfo('template_url'); ?>/assets/img/post-head-1.png" alt="" />
+      </div>
+      <div class="post-head-data">
+        <a class="post-head-data-category" href="">مقالات</a>
+        <h1 class="post-head-data-title">سنسورهای خانواده دی اچ تی</h1>
+        <p class="post-head-data-description">
+          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+          استفاده از طراحان گرافیک است. متن ساختگی با تولید سادگی نامفهوم از
+          صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه
+          روزنامه و مجله در ستون و سطرآنچنان که لازم است
+        </p>
+        <div class="post-head-data-meta-info">
+          <span class="post-head-data-commnet-count">1</span>
+          <span class="post-head-data-publish-date">20 اسفند 1042</span>
+          <span class="post-head-data-auther">نویسنده</span>
+        </div>
+      </div>
+      <div class="clear"></div>
+      <div class="post-inner-data">
+        <p>
+          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+          استفاده از طراحان گرافیک است. متن ساختگی با تولید سادگی نامفهوم از
+          صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه
+          روزنامه و مجله در ستون و سطرآنچنان که لازم است لورم ایپسوم متن
+          ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان
+          گرافیک است. متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+          استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در
+          ستون و سطرآنچنان که لازم است لورم ایپسوم متن ساختگی با تولید سادگی
+          نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. متن ساختگی با
+          تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+          چان بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است لورم ایپسوم
+          متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان
+          گرافیک است. متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+          استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در
+          ستون و سطرآنچنان که لازم است لورم ایپسوم متن ساختگی با تولید سادگی
+          نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. متن ساختگی با
+          تولید سادگی نامفهوم از صنعت چاپ و با استفستون و سطرآنچنان که لازم
+          است لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+          استفاده از طراحان گرافیک است. متن ساختگی با تولید سادگی نامفهوم از
+          صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه
+          روزنامه و مجله در ستون و سطرآنچنان که لازم است لورم ایپسوم متن
+          ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان
+          گرافیک است. متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+          استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در
+          ستون و سطرآنچنان که لازم است ی نامفهوم از صنعت چاپ و با استفاده از
+          طراحان گرافیک است. متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و
+          با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله
+          در ستون و سطرآنچنان که لازم است
+        </p>
+
+        <br />
+      </div>
+    </section>
+    <?php get_sidebar('pgeaside'); ?>
+  </div>
+
+  <?php get_footer('foot'); ?>
+
+</body>
+
+</html>
