81a8b939dbd7086897f0493de5b1c5b8181b737b
hamedvalizadehkelasar <hamedvalizadehkelasar@yahoo.com>
2023-12-29 14:25:11 +0330

    session 21 dynamic sidebar

--- page.php
+++ page.php
@@ -15,7 +15,10 @@
   <?php get_header('menu'); ?>
 
   <div class="recotik-container">
-    <div class="bread-crumb"><?php if (function_exists('rank_math_the_breadcrumbs')) rank_math_the_breadcrumbs(); ?></div>
+    <div class="bread-crumb">
+      <?php if (function_exists('rank_math_the_breadcrumbs'))
+        rank_math_the_breadcrumbs(); ?>
+    </div>
     <section class="page-content">
       <h1 class="page-title">
         <?php the_title(); ?>
@@ -24,9 +27,9 @@
         <?php the_content(); ?>
       </p>
     </section>
-    <?php get_sidebar('pgeaside');?>
+    <?php get_sidebar('pgeaside'); ?>
   </div>
-  
+
   <?php get_footer('foot'); ?>
   <?php get_footer('script-slider'); ?>
 </body>
