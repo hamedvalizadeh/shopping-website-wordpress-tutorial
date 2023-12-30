d926c99d4f9d8c500d76800aba83aecceb4b304a
hamedvalizadehkelasar <hamedvalizadehkelasar@yahoo.com>
2023-12-30 11:17:21 +0330

    session 23-2053 dynamic page 404

---
+++ 404.php
@@ -0,0 +1,28 @@
+<html lang="en" dir="ltr">
+
+<head>
+  <?php get_header('head'); ?>
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/style.css" />
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/menu.css" />
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/header.css" />
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/footer.css" />
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/bread-crumb.css" />
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/not-found.css" />
+</head>
+
+<body>
+  <?php get_header('menu'); ?>
+  <div class="not-found-content">
+    <h1 class="not-found-title">عه مثل اینکه این صفحه وجود نداره</h1>
+    <img src="<?php bloginfo('template_url'); ?>/assets/img/not-found-404.png" alt="" />
+    <p>
+      اشکال نداره. حالا که این صفحه وجود نداره می تونی از صفحه پایین وارد صفحه
+      اصلی بشی
+    </p>
+    <a class="not-found-btn" href="<?php bloginfo('url'); ?>">بریم صفحه اصلی</a>
+  </div>
+
+  <?php get_footer('foot'); ?>
+</body>
+
+</html>
