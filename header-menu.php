27a423f98127ffd4ab90b0d81d10690ac5a2f58e
hamedvalizadehkelasar <hamedvalizadehkelasar@yahoo.com>
2024-02-02 20:11:36 +0330

    session 28-1732: dynamic resigter/login button

--- header-menu.php
+++ header-menu.php
@@ -1,8 +1,24 @@
 <header>
     <a class="btn-search" href="#"></a>
     <a class="btn-basket" href="#"></a>
-    <?php $my_account_page_link = get_permalink(get_option('woocommerce_myaccount_page_id')); ?>
-    <a class="btn-register" href="<?php echo $my_account_page_link; ?>">ورود/ثبت نام</a>
+    <?php
+    $my_account_page_link = get_permalink(get_option('woocommerce_myaccount_page_id'));
+    global $current_user;
+    ?>
+    <a class="btn-register" href="<?php echo $my_account_page_link; ?>">
+        <?php if (is_user_logged_in()) {
+            ?>
+            <div class="register-avatar-container">
+                <?php echo get_avatar($current_user->ID, 25); ?>
+                <p>
+                    <?php echo $current_user->display_name; ?>
+                </p>
+            </div>
+            <?php
+        } else {
+            echo "ورود/ثبت نام";
+        } ?>
+    </a>
     <a class="logo" href="#"><img src="<?php bloginfo("template_url") ?>/assets/img/Logo.png" alt="" /></a>
     <nav class="main-menu">
         <?php wp_nav_menu(
