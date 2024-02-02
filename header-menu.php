a5a331f1833616eccd16adfd731d2ee8da17c1d3
hamedvalizadehkelasar <hamedvalizadehkelasar@yahoo.com>
2024-02-02 17:37:40 +0330

    session 28-0450: set my account page link for login button

--- header-menu.php
+++ header-menu.php
@@ -1,12 +1,15 @@
 <header>
     <a class="btn-search" href="#"></a>
     <a class="btn-basket" href="#"></a>
-    <a class="btn-register" href="#">ورود/ثبت نام</a>
+    <?php $my_account_page_link = get_permalink(get_option('woocommerce_myaccount_page_id')); ?>
+    <a class="btn-register" href="<?php echo $my_account_page_link; ?>">ورود/ثبت نام</a>
     <a class="logo" href="#"><img src="<?php bloginfo("template_url") ?>/assets/img/Logo.png" alt="" /></a>
     <nav class="main-menu">
-        <?php wp_nav_menu(array(
-            'theme_location' => 'header-menu',
-            'container' => ''
-        )); ?>
+        <?php wp_nav_menu(
+            array(
+                'theme_location' => 'header-menu',
+                'container' => ''
+            )
+        ); ?>
     </nav>
 </header>
